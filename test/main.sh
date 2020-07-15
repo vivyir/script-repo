9268617b14b364b35d8070dfc0bf2b5bae4d4ede
phnixir <67435287+phnixir@users.noreply.github.com>
2020-07-15 20:21:05 +0430

    Update main.sh

--- test/main.sh
+++ test/main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# <3
+
 echo "congrats!! your bare client is working dandy finalicious and if you"
 echo "want to make your scripts for bare make sure it follows this init.sh more or less"
 echo "happy scripting!"
